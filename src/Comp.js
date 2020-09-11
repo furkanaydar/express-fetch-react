14d0ee8cbba2ad95d909e361cabf8eda1b932cd6
Furkan Aydar <furkanaydar97@gmail.com>
2020-09-11 17:32:33 +0300

    mess

--- src/Comp.js
+++ src/Comp.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 import DuoOutlinedIcon from '@material-ui/icons/DuoOutlined';
 import axios from 'axios';
+import FileSaver from 'file-saver';
 
 class Comp extends React.Component {
     constructor() {
@@ -12,6 +13,7 @@ class Comp extends React.Component {
         this.state = {
             url: '',
             id: 0,
+            prompt: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.fire = this.fire.bind(this);
@@ -26,26 +28,46 @@ class Comp extends React.Component {
         });
     }
     fire() {
+        var self = this;
+        self.setState({
+            prompt: ''
+        })
         /* https://exp-fetch-furkanaydar.herokuapp.com/ */
         console.log(this.state.url);
         let baseUri = 'https://exp-fetch-furkanaydar.herokuapp.com';
         axios.post(baseUri + '/urls', {
             page: this.state.url,
             vid: this.state.id
-          })
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+        })
+            .then(function (response) {
+                let videoUri = response.data.response;
+                console.log(videoUri);
+                if (videoUri !== 'undefined' && videoUri !== undefined) {
+                    self.setState({
+                        prompt: 'Dosya bulundu. İndirme bittiğinde indirilenler klasöründe gözükecek.'
+                    })
+                    FileSaver.saveAs(videoUri, 'ENESBB.mp4');
+                }
+
+                else {
+                    self.setState({
+                        prompt: 'Dosyaya erişim bloklandı. Bir süreliğine başka video dene.'
+                    }) 
+                }
+
+            })
+            .catch(function (error) {
+                self.setState({
+                    prompt: 'Bir hata oluştu.'
+                })
+            });
     }
     render() {
         return (
-            <div style={{margin: 0, left:'50%', top: '50%', fontFamily: 'Roboto, serif' }}>
+            <div style={{ margin: 0, left: '50%', top: '50%', fontFamily: 'Roboto, serif' }}>
                 <h2 style={{ background: 'yellow', padding: 12, width: 400, margin: 'auto', textAlign: 'center' }}>
                     Video Çalıcı
-                    <a style={{marginLeft:6, verticalAlign: 'middle'}}>
+                    <a style={{ marginLeft: 6, verticalAlign: 'middle' }}>
                         <DuoOutlinedIcon></DuoOutlinedIcon>
 
                     </a>
@@ -59,6 +81,8 @@ class Comp extends React.Component {
                     </input>
                 </div>
                 <button onClick={this.fire} style={{ marginTop: 32, padding: 12, fontSize: 18, letterSpacing: 1.8 }}> İndir</button>
+
+                <div style={{ marginTop: 20, fontSize: 14 }}>{this.state.prompt}</div>
             </div>
         )
     }
